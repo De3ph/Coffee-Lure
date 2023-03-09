f68f47b33af890d1eddd7c0ff1a0659c4732a397
De3ph <05190000108@ogrenci.ege.edu.tr>
2023-03-09 18:06:33 +0300

    AOS lib

--- src/components/Coffees.svelte
+++ src/components/Coffees.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte"
   import scrollreveal from "scrollreveal"
   import { OnScreen } from "@bobbymannino/svelte-on-screen"
 
@@ -25,6 +24,7 @@
   }
 
   import Card from "./Card.svelte"
+  import AosFadeRight from "./Wrappers/AosFadeRight.svelte"
 </script>
 
 <OnScreen bind:showing >
@@ -34,29 +34,36 @@
   >
     <div
     use:assignRef
-    use:animate
       class="grid justify-items-center md:grid-cols-2 m-20 gap-y-10 md:gap-x-10 lg:grid-cols-3 lg:gap-x-7 lg:gap-y-20"
     >
-      <Card
-        imageUrl="/beans/1.webp"
-        title="Lavazza Super Crema Espresso"
-        desc="No one does espresso quite like Italy. The country invented it, then perfected it."
-      />
-      <Card
-        imageUrl="/beans/2.webp"
-        title="SF Bay Coffee Hazelnut Crème, Whole Bean"
-        desc="All kinds of flavored coffees are available today. There’s the seasonal pumpkin spice craze or the always reliable French vanilla route."
-      />
-      <Card
-        imageUrl="/beans/3.webp"
-        title="Kicking Horse Decaf Dark Roast Coffee"
-        desc="A good cup of decaf can be hard to find. When coffee beans go through the decaffeination process, they lose certain chemicals that create the tastes and aromas you love."
-      />
-      <Card
-        imageUrl="/beans/4.webp"
-        title="Seattle's Best Coffee Portside Blend"
-        desc="The only thing better than coffee is a lot of coffee, and Seattle’s Best makes it easy to load up on java with its unbeatable prices."
-      />
+      <AosFadeRight>
+        <Card
+          imageUrl="/beans/1.webp"
+          title="Lavazza Super Crema Espresso"
+          desc="No one does espresso quite like Italy. The country invented it, then perfected it."
+        />
+      </AosFadeRight>
+      <AosFadeRight>
+        <Card
+          imageUrl="/beans/2.webp"
+          title="SF Bay Coffee Hazelnut Crème, Whole Bean"
+          desc="All kinds of flavored coffees are available today. There’s the seasonal pumpkin spice craze or the always reliable French vanilla route."
+        />
+      </AosFadeRight>
+      <AosFadeRight>
+        <Card
+          imageUrl="/beans/3.webp"
+          title="Kicking Horse Decaf Dark Roast Coffee"
+          desc="A good cup of decaf can be hard to find. When coffee beans go through the decaffeination process, they lose certain chemicals that create the tastes and aromas you love."
+        />
+      </AosFadeRight>
+      <AosFadeRight>
+        <Card
+          imageUrl="/beans/4.webp"
+          title="Seattle's Best Coffee Portside Blend"
+          desc="The only thing better than coffee is a lot of coffee, and Seattle’s Best makes it easy to load up on java with its unbeatable prices."
+        />
+      </AosFadeRight>
     </div>
   </main>
 </OnScreen>
---
+++ src/components/Wrappers/AosFadeRight.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import AOS from "aos"
+  import "aos/dist/aos.css"
+  import { onMount } from "svelte"
+  AOS.init()
+</script>
+
+<main data-aos="fade-right" data-aos-easing="ease-in-out" data-aos-once="false">
+  <slot />
+</main>
